d062b0156b22b40c18cf1ce7ef9bb0785f983e02
Jakub Cierocki <jcierocki@gmail.com>
2021-05-18 18:07:30 +0200

    Added other EDA utils, removed work code from hyperparameter_optimization.py

--- models/hyperparameter_optimization.py
+++ models/hyperparameter_optimization.py
@@ -3,17 +3,17 @@ import sys
 # sys.path.append("..")
 
 import fbprophet
-
-from datetime import datetime
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from hyperopt_utils import single_experiment
-from model_utils import prophet, neural_prophet
-from stl_utils import stl_ets
+
+from datetime import datetime
 from hyperopt import fmin, tpe, space_eval, hp
 from hyperopt.pyll.base import scope
-from statsmodels.tsa.seasonal import STL
-import matplotlib.pyplot as plt
+
+from hyperopt_utils import single_experiment
+from model_utils import prophet, neural_prophet
+from eda_utils import stl_ets, optimize_stl
 
 
 def main():
@@ -33,40 +33,6 @@ def main():
     )
 
     print(out)
-
-    def objective(args):
-        return single_experiment(
-            dataframe,
-            "month",
-            datetime.strptime("2017-12-01", "%Y-%m-%d"),
-            "Hard coal consumption per capita [tones]",
-            24,
-            "mae",
-            stl_ets,
-            **args
-        )
-
-    space = {
-        'seasonal': hp.choice('seasonal', [i for i in range(7, 16) if i % 2 != 0]),
-        'trend': hp.choice('trend', [i for i in range(12, 26) if i % 2 != 0]),
-        'low_pass': hp.choice('low_pass', [i for i in range(12, 16) if i % 2 != 0]),
-        'seasonal_deg': hp.choice('seasonal_deg', [0, 1]),
-        'trend_deg': hp.choice('trend_deg', [0, 1]),
-        'low_pass_deg': hp.choice('low_pass_deg', [0, 1])
-    }
-
-    best = fmin(objective, space, algo=tpe.suggest, max_evals=200)
-    best_vals = space_eval(space, best)
-
-    print(best_vals)
-
-    stl_res_default = STL(dataframe["Hard coal consumption per capita [tones]"], period=12, robust=True).fit()
-    stl_res_best = STL(dataframe["Hard coal consumption per capita [tones]"], period=12, robust=True, **best_vals).fit()
-
-    plt.figure()
-    # stl_res_default.plot()
-    stl_res_best.plot()
-    plt.show()
 
 
 if __name__ == "__main__":
